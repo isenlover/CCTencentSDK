346fbcb412b751d911d28e08994c5467666378b0
isenlover <chengce@cc.local>
2017-07-06 11:43:31 +0800

    Initial Commit

---
+++ CCTencentSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCTencentSDK
+//
+//  Created by 程策 on 2017/7/6.
+//  Copyright © 2017年 程策. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CCTencentSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CCTencentSDK
+//
+//  Created by 程策 on 2017/7/6.
+//  Copyright © 2017年 程策. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
